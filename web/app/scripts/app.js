f84f09c5d0d42383bc8a1ef74277d60e3a2fc9bf
Pedro Flores <pflores@dncp.gov.py>
2014-10-17 15:10:34 -0300

    Se persiste correctamente la entidad en el Datastore, ahora necesitamos refrescar el template una vez realizada dicha acción

--- web/app/scripts/app.js
+++ web/app/scripts/app.js
@@ -4,13 +4,14 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+
+    document.querySelector('nueva-frase').addEventListener('frase-agregada', function(e) {
+      var frasesElement = document.querySelector('frases-element');
+      frasesElement.loadFrases();
+    });
+
   });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
-
-function loco(){
-  var xFoo = document.querySelector('frases-add-api');
-  xFoo.llamar();
-}
